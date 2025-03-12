770fecbf2f64f21ab38ebf2d736861b87f8516fb
vinceevogue <ferreiravincent115@gmail.com>
2025-03-12 20:04:17 +0100

    CSS Change

--- Kanbanboard/src/components/ItemCard.jsx
+++ Kanbanboard/src/components/ItemCard.jsx
@@ -5,14 +5,23 @@ export const ItemCard = (props) => {
   return (
     <div className="card-container">
         <h3>{oneKanban.title}</h3>
+
+        <div className='date-container'>
+          <span>{oneKanban.createdDate}</span> - <span>{oneKanban.dueDate}</span>
+        </div>
+
         <p>{oneKanban.description}</p>
-        <p>{oneKanban.assignee}</p>
-        <p>{oneKanban.priority}</p>
-        <p>{oneKanban.createdDate}</p>
-        <p>{oneKanban.dueDate}</p>
-        <button onClick={() => 
+
+        <section>
+          <div>To {oneKanban.assignee}</div>
+          <button onClick={() => 
             deleteTask(oneKanban.id)
         }>Delete</button>
+        </section>
+
+        <div>{oneKanban.priority}</div>
+        
+        
     </div>
     
   )
